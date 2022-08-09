ba9bd93aac289149363914b4544e17912a9ed56d
Clide Stefani <109172241+Monkwire3@users.noreply.github.com>
2022-08-09 14:31:07 -0400

    added knight functionality

--- chess/board.rb
+++ chess/board.rb
@@ -15,9 +15,11 @@ class Board
         @grid[pos[1]][pos[0]] = piece
     end
 
+
     def move_piece(start_pos, end_pos)
-        #debugger
-        #raise "can't move null piece" if @grid[start_pos].is_a?(NullPiece)
+        raise "can't move null piece" if self[start_pos].is_a?(NullPiece)
+        #raise 'there is a piece there' if !self[end_pos].is_a?(NullPiece)
+
         piece = self[start_pos] # grid[0,0] = nil
         self[end_pos] = piece
         self[start_pos] = nil # should be a new Null Piece
@@ -26,9 +28,13 @@ class Board
 end
 
 b = Board.new
-pawn = Piece.new
-b[[0,0]]=pawn
-#p b
+knight = Knight.new(:W, b, [0,0])
+b[[0,0]]=knight
+p b
 p b[[0,0]]
+p '========'
+p b[[0,1]]
 b.move_piece([0,0],[0,1])
 p b
+
+p knight.valid_moves
--- chess/piece.rb
+++ chess/piece.rb
@@ -1,7 +1,65 @@
 class Piece
 
-    def initialize
-        
+    def initialize(color, board, pos)
+        @color = color
+        @board = board
+        @pos = pos
+    end
+
+    def to_s
+    end
+
+    def empty?
+    end
+
+    def valid_moves
+        return []
+    end
+
+    def pos=(val)
+    end
+
+    private
+
+    def move_into_check?(end_pos)
     end
 
 end
+
+class NullPiece < Piece
+    def initialize
+    end
+end
+
+class Pawn < Piece
+
+end
+
+class Rook < Piece
+end
+
+class Bishop < Piece
+end
+
+class Queen < Piece
+end
+
+class King < Piece
+
+end
+
+
+class Knight < Piece
+    def valid_moves
+        deltas = [[-1, -2], [1, -2], [2, -1], [-2, -1], [-2, 1], [2, 1], [-1, 2], [1, 2]]
+
+        possible_moves = deltas.map do |pos|
+            x, y = pos[0], pos[1]
+            [x, y] if (x + @pos[0] <= 8 && y + @pos[1] <= 8 && x + @pos[0] >= 0 && y + @pos[1] >= 0)
+        end.compact
+
+        return possible_moves.each {|m| valid << @board[m].color != @color && !move_into_check?(m)}
+    end
+
+end
+
